0ad7c71c099a7cab9c8078a608dfa1f4474a5e47
Yabe12 <yeabsirabehailu92@gmail.com>
2024-11-22 10:17:29 +0300

    do the first challeng of java done

---
+++ FirstClass/main1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main1{
+  public static void main(String[] args) {
+      Scanner number = new Scanner(System.in);
+      System.out.print("Enter the first number: ");
+      int A = number.nextInt();
+      System.out.print("Enter the second number from the");
+      int B = number.nextInt(); 
+      int Temp = A;
+       A = B;
+       B = Temp;
+      System.out.println("A number is: " + A + " B number is: " + B );
+      number.close();
+  }
+}
+ 
