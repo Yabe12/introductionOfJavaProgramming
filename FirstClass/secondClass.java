d567ec55ccf1a86f42fe162435fc24cfd99804fa
Yabe12 <yeabsirabehailu92@gmail.com>
2024-12-05 14:53:03 +0300

    3rd exer

--- FirstClass/secondClass.java
+++ FirstClass/secondClass.java
@@ -3,16 +3,31 @@ class employ{
   int id;
   String name;
   double salary;
+  public employ(int id, String name, double salary){
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+  }
+  public String getName(){
+    return name;
+  }
+  public int getid(){
+    return id;
+  }
+
+
   public void displayDetails() {
     System.out.println("ID: " + id + ", Name: " + name + ", Salary: " + salary);
 }
 }
 public class secondClass {
   public static void main(String[] args) {
-      employ emp = new employ();
-      emp.id = 1;
-      emp.name = "John Doe";
-      emp.salary = 50000.0;
-     emp.displayDetails ();
+      employ emp = new employ(1,"John",5000);
+ emp.displayDetails ();
+     employ emp1=new employ(2,"haile",5050);
+     System.out.println(emp1.getid());
+
+     emp1.displayDetails();
+
   }
 }
